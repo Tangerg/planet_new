5f2b599b0c448be450c6c243cde6b9f7465540c8
tangerg <tangerg@qq.com>
2024-02-28 14:13:29 +0800

    style: player ui

--- frontend/src/view/components/player/components/left-controls.tsx
+++ frontend/src/view/components/player/components/left-controls.tsx
@@ -1,24 +1,33 @@
 import {FC} from "react";
 import {Image} from "@nextui-org/react";
+
 interface LeftControlsProps {
 
 }
 
-const LeftControls:FC<LeftControlsProps> = (props) =>{
+const Thumbnail: FC = () => {
+    return <div className={"left-controls_thumbnail"}>
+        <Image
+            width={50}
+            height={50}
+            radius={"full"}
+            removeWrapper={true}
+            src="https://nextui-docs-v2.vercel.app/images/album-cover.png"
+        />
+    </div>
+}
+
+const Meta: FC = () => {
+    return <div className={"left-controls_meta"}>
+        <span className={"left-controls_meta-title"}>花田错</span>
+        <span className={"left-controls_meta-artists"}>王力宏</span>
+    </div>
+}
+const LeftControls: FC<LeftControlsProps> = (props) => {
     return <div className={"left-controls-wrapper"}>
         <div className={"left-controls"}>
-            <div className={"left-controls_thumbnail"}>
-                <Image
-                    width={55}
-                    height={55}
-                    alt="Loading"
-                    src="https://app.requestly.io/delay/5000/https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-                />
-            </div>
-            <div className={"left-controls_meta"}>
-                <span className={"left-controls_meta-title"}>花田错</span>
-                <span className={"left-controls_meta-artists"}>王力宏</span>
-            </div>
+            <Thumbnail/>
+            <Meta/>
         </div>
     </div>
 }
--- frontend/src/view/components/player/components/middle-controls.tsx
+++ frontend/src/view/components/player/components/middle-controls.tsx
@@ -1,29 +1,71 @@
 import {FC} from "react";
 import {Slider} from "@nextui-org/react";
+import classnames from "classnames"
+
 interface MiddleControlsProps {
 
 }
 
+interface TimeProps {
+    position: "left" | "right"
+    time: string
+}
 
-const ProgressSlider:FC = () => {
-    return <div className={"middle-controls_actions_slider"}>
+const Time: FC<TimeProps> = (props) => {
+    const {time, position} = props
+    const cls = "middle-controls_progress-slider-time-" + position
+    return <div className={classnames("middle-controls_progress-slider-time", cls)}>
+        {time}
+    </div>
+}
+const ProgressSlider: FC = () => {
+    return <div className={"middle-controls_progress-slider"}>
         <Slider
             size={"sm"}
             step={1}
             defaultValue={0}
             maxValue={100}
             minValue={0}
-            startContent={<div style={{marginRight: "2px"}}>01:23</div>}
-            endContent={<div style={{marginLeft: "2px"}}>03:12</div>}
+            startContent={<Time time={"01:03"} position={"left"}/>}
+            endContent={<Time time={"03:22"} position={"right"}/>}
         />
     </div>
 }
 
 
+const ActionPlayPause: FC = () => {
+    return <div>PlayPause</div>
+}
+
+interface ActionQueueProps {
+    type: "previous" | "next"
+}
+
+const ActionQueue: FC<ActionQueueProps> = (props) => {
+    const {type} = props
+    return <div>
+        {type}
+    </div>
+}
+const ActionPlayMode: FC = () => {
+    return <div>
+        PlayMode
+    </div>
+}
+const Actions: FC = () => {
+    return <div className={"middle-controls_actions"}>
+        <ActionPlayMode/>
+        <ActionQueue type={"previous"}/>
+        <ActionPlayPause/>
+        <ActionQueue type={"next"}/>
+        <ActionPlayMode/>
+    </div>
+}
+
 const MiddleControls: FC<MiddleControlsProps> = (props) => {
     return <div className={"middle-controls-wrapper"}>
         <div className={"middle-controls"}>
-            <div className={"middle-controls_actions_queue"}>Controls</div>
+            <Actions/>
             <ProgressSlider/>
         </div>
     </div>
