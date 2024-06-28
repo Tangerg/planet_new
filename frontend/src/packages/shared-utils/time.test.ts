4451a17cf80a4c2a488f780990a7556f57c9609a
tangerg <tangerg@qq.com>
2024-06-28 21:40:02 +0800

    feat: lyric插件,timer

--- frontend/src/packages/shared-utils/time.test.ts
+++ frontend/src/packages/shared-utils/time.test.ts
@@ -1,7 +1,29 @@
-import {sleep} from "./time";
+import {sleep, Timer} from "./time";
 
 test("sleep", async () => {
     console.log("start", new Date().getTime())
     await sleep(4000)
     console.log("end", new Date().getTime())
+})
+
+test("Timer", async () => {
+    const timer = new Timer()
+    await sleep(500)
+    console.log(timer.duration) //0
+    console.log(timer.isRunning)
+    timer.run()
+    await sleep(1500)
+    console.log(timer.duration) //1500
+    console.log(timer.isRunning)
+    timer.pause()
+    await sleep(500)
+    console.log(timer.duration) //1500
+    console.log(timer.isRunning)
+    timer.run()
+    await sleep(500)
+    console.log(timer.duration) //2000
+    console.log(timer.isRunning)
+    timer.reset()
+    console.log(timer.duration) //0
+    console.log(timer.isRunning)
 })
--- frontend/src/packages/shared-utils/time.ts
+++ frontend/src/packages/shared-utils/time.ts
@@ -3,3 +3,48 @@ export function sleep(duration: number): Promise<void> {
         setTimeout(resolve, duration);
     });
 }
+
+export class Timer {
+    protected state: "running" | "suspended" = "suspended"
+    protected startAt: number = 0
+    protected lastPauseAt: number = 0
+    protected pausedDuration: number = 0
+
+    get isRunning(): boolean {
+        return this.state === "running";
+    }
+
+    get duration(): number {
+        const endAt = this.isRunning ? Date.now() : this.lastPauseAt
+        return endAt - this.startAt - this.pausedDuration;
+    }
+
+    run(): void {
+        if (this.isRunning) {
+            return
+        }
+        const now = Date.now()
+        if (this.startAt === 0) {
+            this.startAt = now
+        }
+        if (this.lastPauseAt !== 0) {
+            this.pausedDuration += (now - this.lastPauseAt)
+        }
+        this.state = "running"
+    }
+
+    pause(): void {
+        if (!this.isRunning) {
+            return
+        }
+        this.lastPauseAt = Date.now()
+        this.state = "suspended"
+    }
+
+    reset(): void {
+        this.state = "suspended"
+        this.startAt = 0
+        this.lastPauseAt = 0
+        this.pausedDuration = 0
+    }
+}
